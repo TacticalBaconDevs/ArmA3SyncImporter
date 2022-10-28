58f83717ee3358b34199212f15b20c485c1cfbf6
shukari <shukari@users.noreply.github.com>
2022-10-28 13:43:35 +0200

    v1.1.0 - assembly is now signed and has a strong name; ArmA3SyncExporter is now updated to the version 20221012; optimized/fixed the readout of the standartoutput; better handling of a failing/stuck process; ParseStandardOutput method is now fixed; renamed the classes for a better difference

---
+++ model/A3SEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace ArmA3SyncImporter.model
+{
+    public class A3SEvent
+    {
+        public string EventName { get; set; } = "";
+
+        public List<string> ModNames { get; set; } = new();
+
+        public override string ToString() => EventName;
+
+    }
+
+}
---
+++ model/A3SMod.cs
@@ -0,0 +1,12 @@
+﻿namespace ArmA3SyncImporter.model
+{
+    public class A3SMod
+    {
+        public string ModName { get; set; } = "";
+
+        public List<A3SPboFile> Files { get; set; } = new();
+
+        public override string ToString() => ModName;
+
+    }
+}
